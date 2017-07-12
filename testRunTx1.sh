ed8cc88de4d5fb2fe5f9e9343eb881a9b0b533da
asmith9455 <asmith9455@gmail.com>
2017-07-12 17:16:46 -0400

    runs on TX1 at about 3fps - probably something I'm doing wrong (should be getting ~15 fps at 640x360

---
+++ testRunTx1.sh
@@ -0,0 +1,5 @@
+python test_seg_v2_video_v2.py --model /media/ubuntu/tx1_sd/repos/ENet/prototxts/enet_deploy_final_640x360.prototxt --weights /media/ubuntu/tx1_sd/repos/ENet/enet_weights_zoo/cityscapes_weights.caffemodel \
+--colours /media/ubuntu/tx1_sd/repos/ENet/scripts/cityscapes19.png \
+--input_image /home/ubuntu/Downloads/20170710_153711.mp4 \
+--out_dir /media/ubuntu/tx1_sd/repos/ENet/example_image/out.bmp \
+--gpu=1
--- test_seg_v2_video_v2.py
+++ test_seg_v2_video_v2.py
@@ -14,15 +14,14 @@ import time
 print "PATH FOR IMPORTING NUMPY:"
 print os.environ["PYTHONPATH"]
 
-caffe_root = '/home/alex/repos/ENet/caffe-enet/'  # Change this to the absolute directory to ENet Caffe
+caffe_root = '/media/ubuntu/tx1_sd/repos/ENet/caffe-enet/'  # Change this to the absolute directory to ENet Caffe (TX1)
+#caffe_root = '/home/alex/repos/ENet/caffe-enet/'  # Change this to the absolute directory to ENet Caffe (Desktop)
 
 import subprocess
 
-
-caffe_root = '/home/alex/repos/ENet/caffe-enet/'  # Change this to the absolute directory to ENet Caffe
 sys.path.insert(0, caffe_root + 'python')
 
-os.environ["PYTHONPATH"] = "/home/alex/repos/ENet/caffe-enet/python/:/media/ubuntu/tx1_sd/repos/ENet/caffe-enet/python/caffe";
+os.environ["PYTHONPATH"] = caffe_root+"python/:"+caffe_root+"python/caffe/";
 print "PATH FOR IMPORTING CAFFE:"
 print os.environ["PYTHONPATH"]
 
@@ -54,8 +53,11 @@ def make_parser():
 if __name__ == '__main__':
     parser1 = make_parser()
     args = parser1.parse_args()
-    if args.gpu == 0:
+
+    gpuMode = False
+    if args.gpu == 0 or True:
         caffe.set_mode_gpu()
+	gpuMode = True
     else:
         caffe.set_mode_cpu()
 
@@ -69,23 +71,24 @@ if __name__ == '__main__':
     videoPath = args.input_image #"/home/alex/Downloads/20170710_153711.mp4"
     cap = cv2.VideoCapture(videoPath)
 
-    pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+    pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
     frameskip = 100
     framectr = 0
     print "frameskip is " + str(frameskip) 
     while True:
 	
+	#pdb.set_trace()
 	startTime = int(round(time.time()*1000))
 	flag, frame = cap.read()
 	framectr += 1
 	if flag:
 	    # The frame is ready and already captured
 	    cv2.imshow('video', frame)
-	    pos_frame = cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+	    pos_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
 	    
 	else:
 	    # The next frame is not ready, so we try to read it again
-	    cap.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, pos_frame-1)
+	    cap.set(1, pos_frame-1)
 	    print "frame is not ready"
 	    # It is better to wait for a while for the next frame to be ready
 	    cv2.waitKey(1000)
@@ -94,7 +97,7 @@ if __name__ == '__main__':
         if cv2.waitKey(10) == 27:
             break
 
-        if cap.get(cv2.cv.CV_CAP_PROP_POS_FRAMES) == cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT):
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
             # If the number of captured frames is equal to the total number of frames,
             # we stop
             break
@@ -104,27 +107,35 @@ if __name__ == '__main__':
 	
 
 	framectr += 1
+
 	if (framectr % frameskip != 0):
 		continue
+
+	#pdb.set_trace()
 	print str(pos_frame)+" frames"
 	print "image capture time is: " + str(endTime- startTime) + "ms"
 	
 	startTime = int(round(time.time()*1000))
-
-	input_image = cv2.resize(frame, (640,480), interpolation = cv2.INTER_CUBIC)
+	input_image = frame
+	#input_image = cv2.resize(frame, (640,480), interpolation = cv2.INTER_CUBIC)
 
 	input_image = cv2.resize(input_image, (input_shape[3], input_shape[2]))
 	input_image = input_image.transpose((2, 0, 1))
 	input_image = np.asarray([input_image])
 
+	print "prepared ip image"
+	print "ip image shape is: " + str(input_image.shape)
 
         startTime2 = int(round(time.time()*1000))
 	out = net.forward_all(**{net.inputs[0]: input_image})
 	endTime2 = int(round(time.time()*1000))
+	#pdb.set_trace()
+	print "finished forward pass"
 	prediction = net.blobs['deconv6_0_0'].data[0].argmax(axis=0)
 	endTime3 = int(round(time.time()*1000))
-        
-
+       
+	print 'finished prediction extraction'
+	#pdb.set_trace()
 
 	prediction = np.squeeze(prediction)
 	prediction = np.resize(prediction, (3, input_shape[2], input_shape[3]))
@@ -135,18 +146,28 @@ if __name__ == '__main__':
 	cv2.LUT(prediction, label_colours_bgr, prediction_rgb)
 
 	endTime = int(round(time.time()*1000))
-	print "processing time is: " + str(endTime- startTime) + "ms"
+	if (gpuMode):
+		print "processing time is: " + str(endTime- startTime) + "ms (gpu mode)"
+	else:
+		print "processing time is: " + str(endTime- startTime) + "ms (cpu mode)"
 	print "of this, nn fwd propagation time is: " + str(endTime2- startTime2) + "ms"
 	print "of this, nn data rearrangement is: " + str(endTime3- endTime2) + "ms"
 
 
 	startTime = int(round(time.time()*1000))
 	cv2.imshow("ENet", prediction_rgb)
+
+	print "prediction image shape is: " + str(prediction_rgb.shape)
+	
 	endTime = int(round(time.time()*1000))
 
 	print "display time is: " + str(endTime- startTime) + "ms"
 
-    if args.out_dir is not None:
+	print "not waiting for keypress"
+	#cv2.waitKey(0)
+	print "didn't get keypress"
+
+    if (args.out_dir is not None) and False:
         input_path_ext = args.input_image.split(".")[-1]
         input_image_name = args.input_image.split("/")[-1:][0].replace('.' + input_path_ext, '')
         out_path_im = args.out_dir + input_image_name + '_enet' + '.' + input_path_ext
